9be54255a258b865811c77d7cca31ae0e5007fa4
timmyreilly <timreilly@live.com>
2016-01-08 14:28:42 -0800

    added logic

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -5,13 +5,18 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject import app
+from tts import get_raw_wav
 
 @app.route('/')
-@app.route('/home')
-def home():
-    """Renders the home page."""
+def my_form():
+    return render_template("my-form.html")
+
+@app.route('/', methods=['POST']) 
+def my_form_post():
+    text = request.form['text'] 
+    wav = get_raw_wav(text) 
     return render_template(
         'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
+        title = 'New Phrase',
+        wavfile = wav 
+        )
